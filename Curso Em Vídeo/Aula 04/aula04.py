d8c176081dd704d857b344f8683dbbab1f65c6bc
AdemirRamos <ademirramos634@gmail.com>
2022-01-15 21:23:49 -0300

    Adição da Aula 04

---
+++ Curso Em Vídeo/Aula 04/aula04.py
@@ -0,0 +1,16 @@
+#Operadores Lógicos e Ralacionais:
+
+#>: maior que; <: menor que; >=: maior ou igual a; <=: menor igual a; =: igual a; <>: diferente de.
+
+#Operadores Lógicos:
+
+#"E", "Ou" e "NÃO".
+
+#"NÃO" inverte o valor "boolean".
+
+#IMPORTANTE: Comparações relacionais devem ir entre parênteses - pois só assim elas geram resultados lógicos.
+
+#Ordem de Precedência Geral: 1ª: operadores aritméticos; 2ª: operadores relacionais; 3ª: operações lógicas (ordem: "E", "OU", "NÃO").
+
+#P. S.: Operadores relacionais são resolvidos na ordem em que aparecem.
+
