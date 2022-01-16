43143e139bf96a59c3946f35e260a7e97f5f0daf
AdemirRamos <ademirramos634@gmail.com>
2022-01-15 21:00:34 -0300

    Adição da Aula 03

---
+++ Curso Em Vídeo/Aula 03/aula03.py
@@ -0,0 +1,34 @@
+#Comandos de Entrada:
+
+#- Servem para receber dados enviados por parte do (s) usuário (s).
+
+#No Visualg, o comando "leia()" é um comando de entrada.
+
+#- Exemplo: leia(variável).
+
+#Exercício prático requisitado nesta aula: socile ao usuário dois número e realize a soma entre eles.
+
+#Resolução:
+
+n1 = int(input('Digite um número: ')); n2 = int(input('Digite mais um número: '))
+print(f'O resultado da soma entre os dois números digitados é: {n1 + n2}')
+
+#Operadores Aritméticos (Visualg):
+
+#+: adição; -: subtração; *: multiplicação; /: divisão; \: divisão inteira; ^: exponenciação. %: módulo.
+
+#Divisão inteira: o número que divide; Módulo: resto da divisão.
+
+#Ordem de precedência: 1ª: (); 2ª: ^; 3ª: * e /; 4ª: + e -.
+
+#Funções Aritméticas do Visualg:
+
+#Abs: valor absoluto;
+#Exp: exponenciação;
+#Int: valor inteiro;
+#RaizQ: raiz quadrada;
+#Pi: retorna pi;
+#Sen: seno (radiano);
+#Cos: cosseno (radiano);
+#Tan: tangente (radiano);
+#GraupRad: graus para radianos.
