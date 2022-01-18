480c5a28246194fa2140d426b66c6791d596ec48
AdemirRamos <ademirramos634@gmail.com>
2022-01-18 01:03:07 -0300

    Atualização da Aula 06

--- Curso Em Vídeo/Aula 06/aula06.py
+++ Curso Em Vídeo/Aula 06/aula06.py
@@ -19,8 +19,17 @@ temperature = float(input('Digite a temperatura em ºF: '))
 fórmula = (temperature - 32) * 5 / 9
 print(f'A temperatura em ºC é igual a: {fórmula:.2f}.')
 
-#4ª: Porcentagem.
+#4ª: Porcentagem;
 
 valor = float(input('Digite o valor das compras: '))
 cálculo = valor * 60 / 100
 print(f'O total de imposto a ser pago é: R${cálculo:.2f}.')
+
+#5ª: Empréstimo.
+
+valor = float(input('Digite o valor: R$'))
+parcelas = int(input('Digite o número de parcelas: '))
+cálculo = (valor * 20) / 100
+resultado = cálculo + valor
+resultado_final = resultado / parcelas
+print(f'O valor das suas parcelas será de: R${resultado_final:.2f}.')
